cdca8f3101030d22b590a2fde8d816e72f87de0c
onlinearunava <onlinearunava@gmail.com>
2022-07-15 19:14:24 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import './main.css';
+import Invoice from './routes/invoice';
+
+// let rootElement = document.getElementById('root');
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   rootElement
+// );
+
+// const root = ReacDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={<main style={{ padding: "1rem" }}>
+            <p>Select an invoice</p>
+          </main>}></Route>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
